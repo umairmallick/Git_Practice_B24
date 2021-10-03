e94e9b0498763fa4fbab057a819fc2088baaabdb
Malik <aamiramallick7@gmail.com>
2021-10-03 12:44:14 -0400

    3rd file 3rd commit

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,6 +4,7 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("This is the 3rd file 1st commit");
         System.out.println("This is the 3rd file 2nd commit");
+        System.out.println("This is the 3rd file 3rd commit");
 
     }
 }
